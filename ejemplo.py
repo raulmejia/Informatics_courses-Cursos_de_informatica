59f5b53dd8b7bd3de87af9b4aac80834ed38a5e8
Rodrigo Garcia <absorto@sdf.org>
2014-03-07 14:29:10 -0600

    agrego tantito codigo de python para mostrar algunas caracteristicas

---
+++ ejemplo.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+lista = ['a', "b", 3, 234.23]
+
+diccionario = { 'llave': "valor",
+                'nombre': "Rodrigo",
+                'estatura': 1.72,
+                'items': lista }
+
+
+for llave in diccionario.keys():
+    print diccionario[llave]
+
+
+for e in lista:
+    if e == 'a':
+        print e
+
+count = 0
+while (count < 9):
+   print 'The count is:', count
+   count = count + 1
+
+
+for n in range(1,20):
+    print n
+
+print diccionario['nombre'].upper()
+
+
+
+# import fileinput
+
+# for line in fileinput.input(args):
+#     print line
