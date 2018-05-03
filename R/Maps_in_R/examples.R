82e9dc26e2d3e3266233b55c04304e1ebe95d5f7
raulmejia <raulmejia@users.noreply.github.com>
2018-05-03 17:03:41 -0500

    Update examples.R

--- R/Maps_in_R/examples.R
+++ R/Maps_in_R/examples.R
@@ -1,2 +1,17 @@
 https://stackoverflow.com/questions/29907053/plot-colour-coded-world-map-using-ggplot2  
-https://journal.r-project.org/archive/2011-1/RJournal_2011-1_South.pdf
+https://journal.r-project.org/archive/2011-1/RJournal_2011-1_South.pdf  
+Maps from Worls economic forum:  
+http://r-video-tutorial.blogspot.mx/2015/05/global-economic-maps.html  
+http://r-video-tutorial.blogspot.mx/2015/05/global-economic-maps.html  
+http://r-video-tutorial.blogspot.mx/2015/05/interactive-maps-for-web-in-r.html  
+
+ 34  apt-get install libgdal20  
+   35  apt search gdal  
+   36  apt-get install gdal-bin libgdal-dev  
+   install.packages(c("rgdal","XML","raster"))  
+   library(raster)  
+  
+
+
+
+https://www.computerworld.com/article/3038270/data-analytics/create-maps-in-r-in-10-fairly-easy-steps.html
