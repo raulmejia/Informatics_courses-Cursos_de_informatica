f804309fbfaa6b7c6ff0de00ae18658279ec7b6f
raulmejia <raulmejia@users.noreply.github.com>
2018-05-03 14:29:58 -0500

    Update examples.R

--- R/Maps_in_R/examples.R
+++ R/Maps_in_R/examples.R
@@ -1,1 +1,2 @@
-https://stackoverflow.com/questions/29907053/plot-colour-coded-world-map-using-ggplot2
+https://stackoverflow.com/questions/29907053/plot-colour-coded-world-map-using-ggplot2  
+https://journal.r-project.org/archive/2011-1/RJournal_2011-1_South.pdf
