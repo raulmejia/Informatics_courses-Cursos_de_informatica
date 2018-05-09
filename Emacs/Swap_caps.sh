dba450f9a8d939e0e235b31b9c5fee48132be71c
raulmejia <raulmejia@users.noreply.github.com>
2018-05-08 19:04:35 -0500

    Update Swap_caps.sh

--- Emacs/Swap_caps.sh
+++ Emacs/Swap_caps.sh
@@ -1,4 +1,7 @@
 
+#!/bin/bash
+# Este programa intercambia la tecla Mayúsculas por control (ambas del lado izquierdo del teclado) y establece el teclado "es"
 sed -i '/XKBLAYOUT/c\XKBLAYOUT="es"' /etc/default/keyboard
 sed -i '/XKBOPTIONS/c\XKBOPTIONS="ctrl:swapcaps"' /etc/default/keyboard
 sudo dpkg-reconfigure -phigh console-setup
+# ahora sòlo falta reiniciar la computadora
