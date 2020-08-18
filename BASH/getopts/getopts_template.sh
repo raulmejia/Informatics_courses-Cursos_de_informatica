ebf4934f79ce3365b3a5dda18e16b0f72e6c8c84
raulmejia <raulmejia@users.noreply.github.com>
2020-08-18 12:33:13 +0200

    Create getopts_template.sh

---
+++ BASH/getopts/getopts_template.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+### For the options given by the user 
+OPTIND=1 # Reset if getopts used previously
+
+if [ -z "$1" ]
+  then
+    echo "No argument supplied, usage is ..."
+fi
+
+while getopts ":h:u:p:d:" opt; do
+        case "$opt" in
+
+                h)
+                        MYSQL_HOST=$OPTARG
+                        ;;
+                u)
+                        MYSQL_USER=$OPTARG
+                        ;;
+                p)
+                        MYSQL_PASS=$OPTARG
+                        ;;
+                d)
+                        BACKUP_DIR=$OPTARG
+                        ;;
+                \?)
+                        echo "Invalid option: -$OPTARG" >&2
+                        exit 2;;
+                :)
+                       echo "Option -$OPTARG requires an argument" >&2
+                       exit 2;;
+        esac
+done
+shift $((OPTIND-1))
+echo "MYSQL_HOST='$MYSQL_HOST'  MYSQL_USER='$MYSQL_USER'  MYSQL_PASS='$MYSQL_PASS'  BACKUP_DIR='$BACKUP_DIR' Additionals: $@"
+
+if [ -z "$MYSQL_HOST" ]; then
+    echo "option a was NOT given, exit. Usage: ---- " >&2
+    exit 2;
+fi
+
+### For the options given by the user 
