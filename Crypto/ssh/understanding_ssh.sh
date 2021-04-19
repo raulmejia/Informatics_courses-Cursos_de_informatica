2e2941d9c7fdcd130c7d828b6c8d969dfc5bc4e9
raulmejia <raulmejia@users.noreply.github.com>
2021-04-19 12:40:15 +0200

    Update understanding_ssh.sh

--- Crypto/ssh/understanding_ssh.sh
+++ Crypto/ssh/understanding_ssh.sh
@@ -1,1 +1,34 @@
 
+working with ssh keys
+	coping your sshkey to the server https://www.youtube.com/watch?v=5Fcf-8LPvws
+
+https://www.youtube.com/watch?v=vpk_1gldOAE
+ssh-keygen -t rsa -b 1096
+
+scp ~/.ssh/id_rsa.pub someuser@ip.host:/folder/someuser
+scp ~/.ssh/id_rsa.pub someuser@ip.host:/folder/someuser/.ssh/uploaded_key.pub
+
+	# or your could use 
+
+
+#######################
+# On the server #####
+#########################
+cat ~/.ssh/uploaded_key.pub >> ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/*
+
+
+# deactivating pasword authentication in linux
+/etc/ssh/sshd_config # always make a backup   cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bk
+
+########  Local #####
+
+connect through ssh
+
+### explanation of ssh
+https://www.youtube.com/watch?v=ORcvSkgdA58
+
+rsa public key
+TLS
+telnet
+SSH (SCP & SFTP)
